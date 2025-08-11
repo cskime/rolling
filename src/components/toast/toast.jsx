7f7d3db9929aa127fae6300c37924afc1a7f9af9
Chamsol Kim <kcsol1005@gmail.com>
2025-08-11 16:31:36 +0900

    feat [#8] `Toast`를 일정 시간 보여주고 제거하는 `useToast` custom hook 구현

--- src/components/toast/toast.jsx
+++ src/components/toast/toast.jsx
@@ -11,15 +11,31 @@ const StyledToast = styled.div`
   display: flex;
   align-items: center;
   gap: 12px;
-
   font-size: 16px;
   font-weight: 400;
   line-height: 26px;
   color: white;
+  white-space: nowrap;
+  position: fixed;
+  left: 50%;
+  bottom: 70px;
+  transform: translateX(-50%);
 
   p {
     margin: 0;
     flex-grow: 1;
+  }
+
+  @media (max-width: 1199px) {
+    bottom: 50px;
+  }
+
+  @media (max-width: 767px) {
+    min-width: 0;
+    transform: none;
+    left: 20px;
+    right: 20px;
+    bottom: 88px;
   }
 `;
 
