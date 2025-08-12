32530cda2d6fc268a1e036f050f992f140efd33a
Chamsol Kim <kcsol1005@gmail.com>
2025-08-12 21:52:00 +0900

    feat [#15] `OnboardingLayout` 내부에서 '롤링 페이퍼 만들기' 버튼 클릭 시 직접 '/post'로 이동

--- src/layouts/onboarding-layout.jsx
+++ src/layouts/onboarding-layout.jsx
@@ -1,15 +1,20 @@
+import { useNavigate } from "react-router";
 import { OutlinedButton } from "../components/button/button";
 import BUTTON_SIZE from "../components/button/button-size";
 import Header from "../components/header/header";
 
-function OnboardingLayout({ children, onCreate }) {
+function OnboardingLayout({ children }) {
+  const navigate = useNavigate();
+
+  const handlePostCreate = () => navigate("/post");
+
   return (
     <>
       <Header>
         <OutlinedButton
           size={BUTTON_SIZE.medium}
           title="롤링 페이퍼 만들기"
-          onClick={onCreate}
+          onClick={handlePostCreate}
         />
       </Header>
       <main>{children}</main>
