8a82af25ac16d663550e22385775721de3031ed9
Chamsol Kim <kcsol1005@gmail.com>
2025-08-12 20:05:53 +0900

    feat [#10] Modal의 전역 상태를 관리하는 context 및 provider 구현

--- src/app.jsx
+++ src/app.jsx
@@ -1,18 +1,27 @@
 import { BrowserRouter, Route, Routes } from "react-router";
+import ModalProvider from "./components/modal/modal-provider";
 import DropdownProvider from "./components/text-field/dropdown-input/dropdown-provider";
 import MessagePage from "./pages/message-list";
 import TestPage from "./pages/test-page";
 
+function Provider({ children }) {
+  return (
+    <ModalProvider>
+      <DropdownProvider>{children}</DropdownProvider>
+    </ModalProvider>
+  );
+}
+
 function App() {
   return (
-    <DropdownProvider>
+    <Provider>
       <BrowserRouter>
         <Routes>
           <Route path="/test-components" element={<TestPage />} />
           <Route path="/list" element={<MessagePage />} />
         </Routes>
       </BrowserRouter>
-    </DropdownProvider>
+    </Provider>
   );
 }
 
