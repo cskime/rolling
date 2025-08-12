e0745c4767800f8b4536e4a89d1c71a0eccf37b5
Chamsol Kim <kcsol1005@gmail.com>
2025-08-12 16:37:19 +0900

    fix [] Missing import 수정

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import DropdownProvider from "./components/text-field/dropdown-input/dropdown-provider";
+import MessagePage from "./pages/message-list";
 import TestPage from "./pages/test-page";
 
 function App() {
