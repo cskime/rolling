3afd179a81a6ad17325f6fc8623bccd7d3c4fb4b
Chamsol Kim <kcsol1005@gmail.com>
2025-08-14 22:41:31 +0900

    feat [#34] `MessagesPage` sub header에 message 추가, 공유 버튼 구현

--- src/features/rolling-paper/components/rolling-paper-header.jsx
+++ src/features/rolling-paper/components/rolling-paper-header.jsx
@@ -1,4 +1,8 @@
 import styled from "styled-components";
+import addImage from "../../../assets/ic-face-smile-add.svg";
+import shareImage from "../../../assets/ic-share.svg";
+import { OutlinedButton } from "../../../components/button/button";
+import BUTTON_SIZE from "../../../components/button/button-size";
 import Colors from "../../../components/color/colors";
 import { media } from "../../../utils/media";
 
@@ -31,6 +35,39 @@ const RecipientName = styled.h2`
   color: ${Colors.gray(800)};
 `;
 
+const Divider = styled.div`
+  width: 1px;
+  height: 28px;
+  background-color: ${Colors.gray(200)};
+`;
+
+const StyledDividedContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 28px;
+`;
+
+function DividedContainer({ children }) {
+  return (
+    <StyledDividedContainer>
+      {children[0]}
+      <Divider />
+      {children[1]}
+    </StyledDividedContainer>
+  );
+}
+
+const HeaderTrailing = styled.div`
+  display: flex;
+  gap: 8px;
+  align-items: center;
+`;
+
+const ShareButton = styled(OutlinedButton)`
+  width: auto;
+  padding: 0 16px;
+`;
+
 function RollingPaperHeader({ recipientName }) {
   return (
     <StyledRollingPaperHeader>
@@ -38,6 +75,16 @@ function RollingPaperHeader({ recipientName }) {
         <div>
           <RecipientName>{`To. ${recipientName}`}</RecipientName>
         </div>
+        <HeaderTrailing>
+          <DividedContainer>
+            <OutlinedButton
+              size={BUTTON_SIZE.small}
+              title="추가"
+              icon={addImage}
+            />
+            <ShareButton size={BUTTON_SIZE.small} icon={shareImage} />
+          </DividedContainer>
+        </HeaderTrailing>
       </HeaderContent>
     </StyledRollingPaperHeader>
   );
