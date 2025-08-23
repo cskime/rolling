f521144e6c8792f95db07b89a1efdd68e6c6014a
luli <nidor022@gmail.com>
2025-08-23 15:14:46 +0900

    [#80] 코드 오타 수정
     코드 오타 수정

--- src/pages/rolling-paper-list-page.jsx
+++ src/pages/rolling-paper-list-page.jsx
@@ -1,8 +1,8 @@
 import { PrimaryButton } from "../components/button/button";
 import BUTTON_SIZE from "../components/button/button-size";
-import { useNavigate } from "react-router";
+import { getRollingPaperList } from "../features/rolling-paper/api/rolling-paper-list";
 import React, { useEffect, useState, useMemo } from "react";
-import { getRollingPaperList } from "../features/rolling-paper/api/rollingPaperList";
+import { useNavigate } from "react-router";
 import styled from "styled-components";
 import RollingPaperList from "../features/rolling-paper/components/rolling-paper-list";
 import { media } from "../utils/media";
